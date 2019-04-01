bc014d3ae4f5da17ad175328c7d6387986510b33
Rudy Zhou <Rudy.Zhou@td.com>
2019-04-01 15:29:06 -0400

    added mongoose movie schema

---
+++ models/movie.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var movieSchema = mongoose.Schema({
+  name: {type: String},
+  director: {type: String},
+  writers: {type: String},
+  country: {type: String},
+  type: {type: String},
+  cast: {type: String},
+  image: {type: String},
+  rater: [{
+    raterEmail: {type: String, default: ''},
+    rateFullName: {type: String, default: ''}
+  }],
+
+  movieRating: [{
+    movieName: {type: String, default: ''},
+    userFullName: {type: String, default: ''},
+    movieImage: {type: String, default: ''},
+    userRating: {type: Number, default: 0},
+    userReview: {type: String, default: 0}
+  }],
+
+  ratingNumber: [Number],
+  ratingSum: {type: Number, default: 0}
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
