2b02fa17fab5cb6f89c2751a002d73c8801727fa
Rudy Zhou <Rudy.Zhou@td.com>
2019-04-01 16:34:15 -0400

    added post route for movie creation and saving logic

--- public/js/validate.js
+++ public/js/validate.js
@@ -9,42 +9,42 @@ $(document).ready(function(){
     var img = $.trim($('#upload-input').val());
 
     var isValid = true;
-    if(name -- ''){
+    if(name == ''){
       isValid = false;
       $('#errorMsg1').html('<div class="alert alert-danger">Name field is empty</div>');
     } else {
       $('#errorMsg1').html('');
     }
 
-    if(director -- ''){
+    if(director == ''){
       isValid = false;
       $('#errorMsg2').html('<div class="alert alert-danger">director field is empty</div>');
     } else {
       $('#errorMsg2').html('');
     }
 
-    if(writers -- ''){
+    if(writers == ''){
       isValid = false;
       $('#errorMsg3').html('<div class="alert alert-danger">writers field is empty</div>');
     } else {
       $('#errorMsg3').html('');
     }
 
-    if(country -- ''){
+    if(country == ''){
       isValid = false;
       $('#errorMsg4').html('<div class="alert alert-danger">country field is empty</div>');
     } else {
       $('#errorMsg4').html('');
     }
 
-    if(type -- ''){
+    if(type == ''){
       isValid = false;
       $('#errorMsg5').html('<div class="alert alert-danger">type field is empty</div>');
     } else {
       $('#errorMsg5').html('');
     }
 
-    if(cast -- ''){
+    if(cast == ''){
       isValid = false;
       $('#errorMsg6').html('<div class="alert alert-danger">cast field is empty</div>');
     } else {
@@ -58,9 +58,10 @@ $(document).ready(function(){
         writers: writers,
         country: country,
         type: type,
-        cast: case,
+        cast: cast,
         img: img
       };
+
       $.ajax({
         url: '/movie/create',
         type: 'POST',
@@ -75,7 +76,7 @@ $(document).ready(function(){
         }
       })
     }else{
-
+      return false;
     }
 
   });
--- routes/movie.js
+++ routes/movie.js
@@ -1,10 +1,33 @@
 var formidable = require('formidable');
 var path = require('path');
 var fs = require('fs');
+var Movie = require('../models/movie');
 
 module.exports = (app) => {
   app.get('/movie/create', (req, res) => {
-    res.render('movie/movie', {title: 'Movie Registration', user: req.user});
+    var success = req.flash('success');
+    res.render('movie/movie', {title: 'Movie Registration', user: req.user, success:success, noErrors: success.length > 0});
+  });
+
+  app.post('/movie/create', (req, res) => {
+    // add server side validation
+    var newMovie = new Movie();
+    newMovie.name = req.body.name;
+    newMovie.director = req.body.director;
+    newMovie.writers = req.body.writers;
+    newMovie.country = req.body.country;
+    newMovie.type = req.body.type;
+    newMovie.cast = req.body.cast;
+    newMovie.image = req.body.upload;
+
+    newMovie.save((err) => {
+      if(err){
+        console.log(err);
+      }
+      console.log(newMovie);
+      req.flash('success', 'Movie data has been successfully added');
+      res.redirect('/movie/create');
+    })
   });
 
   app.post('/upload', (req, res) => {
